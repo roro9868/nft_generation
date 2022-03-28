3fef567e250edc6b3d224f1ac73271a5e0f27fc0
Ro <99719677+roro9868@users.noreply.github.com>
2022-03-28 12:45:24 -0700

    Some config changes to generate nfts

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace";
+const namePrefix = "Great Eyes";
+const description = "Fairly good eyes collection";
+const baseUri = "ipfs://QmZL2GwEQLGYMfHQMJXAMC3EsTNUG4n9YXLne4QkwMxpBR";
 
 const solanaMetadata = {
   symbol: "YC",
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 200,
     layersOrder: [
       { name: "Background" },
       { name: "Eyeball" },
